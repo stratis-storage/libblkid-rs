a2939cf9381a473428d169f71e371b46f4b6dc5c
John Baublitz <jbaublitz@redhat.com>
2020-08-10 15:10:29 -0400

    Added license and license headers

--- src/topology.rs
+++ src/topology.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 /// A handle for traversing the blkid topology of devices.
 pub struct BlkidTopology(libblkid_rs_sys::blkid_topology);
 
